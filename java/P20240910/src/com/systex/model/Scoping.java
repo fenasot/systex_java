764afa6417a9ee653c9b51334e006cdfd78b0ee1
fena <fenahave@gmail.com>
2024-09-10 10:11:49 +0800

    20240910 第一個更新

---
+++ java/P20240910/src/com/systex/model/Scoping.java
@@ -0,0 +1,8 @@
+package com.systex.model;
+
+public class Scoping {
+	public static int a; //Class Variable
+	public int b; //Member Variable
+	//
+	
+}
---
+++ java/P20240910/src/com/systex/model/TestScoping.java
@@ -0,0 +1,9 @@
+package com.systex.model;
+
+public class TestScoping {
+	
+	public static void main(String[] args) {
+		System.out.print(Scoping.a);
+	}
+	
+}
