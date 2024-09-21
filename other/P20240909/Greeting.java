2f4fce53537e75df7f28fc64852135e2b4a07cc3
fena <79359861+fenasot@users.noreply.github.com>
2024-09-21 16:21:31 +0800

    刪除不必要資料

--- other/P20240909/Greeting.java
+++
@@ -1,6 +0,0 @@
-//This is a non-excutable Java Class
-public class Greeting{
-	public void sayHello(String name) {
-		System.out.println("Hello, " +name);
-	}
-}
--- other/P20240909/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-// This is an executable Java Class
-public class HelloWorld{
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		Greeting g = new Greeting();
-		g.sayHello("Sparrow");
-	}
-}
